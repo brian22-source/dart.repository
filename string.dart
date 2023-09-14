5d5a9a45219089d70aae4b2dd54e350df7cf88e0
19carty <143806608+19carty@users.noreply.github.com>
2023-09-14 15:38:49 +0300

    first string dart

---
+++ string.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Brian";
+  String city = "Nairobi";
+  String leisure = "playing games";
+
+  print("my name is $name");
+  print("my leisure time i like $leisure");
+}
