d001675fd36094fad9a32be2c74bb6ee5a62c21d
19carty <143806608+19carty@users.noreply.github.com>
2023-09-14 15:58:19 +0300

    string buffer

---
+++ stringgreeting.dart
@@ -0,0 +1,10 @@
+void main() {
+  StringBuffer buffer = StringBuffer();
+
+  buffer.write("brian");
+  buffer.write(" ");
+  buffer.write("mbithi");
+
+  String result = buffer.toString();
+  print(result);
+}
