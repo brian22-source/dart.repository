2f106342eb30e642097026d708ada3dcec1f6aab
19carty <143806608+19carty@users.noreply.github.com>
2023-09-14 16:06:27 +0300

    integer

---
+++ integer.dart
@@ -0,0 +1,16 @@
+void main() {
+  int age = 25;
+  int temperature = 75;
+
+  int sum = age + temperature;
+  int difference = age - temperature;
+  int product = age * temperature;
+  double quotient = age / temperature;
+
+  print("Age: $age");
+  print("Temperature: $temperature");
+  print("Sum: $sum");
+  print("Difference: $difference");
+  print("Product: $product");
+  print("Quotient: $quotient");
+}
