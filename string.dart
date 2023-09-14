b61fcfc719ccb9764c249f4c96d3e27522e98f48
19carty <143806608+19carty@users.noreply.github.com>
2023-09-14 15:46:17 +0300

    changes made to my functions

--- string.dart
+++ string.dart
@@ -4,5 +4,5 @@ void main() {
   String leisure = "playing games";
 
   print("my name is $name");
-  print("my leisure time i like $leisure");
+  print(" i like $leisure");
 }
---
+++ string2.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Brian";
+  String city = "Nairobi";
+  String leisure = "Playing games";
+
+  print(" my name is $name and my city is $city . and i like $leisure");
+  print(i like)
+}
