31329b05a1bd5aa15ca97230bb2257c4f54ed8e3
19carty <143806608+19carty@users.noreply.github.com>
2023-09-15 15:38:09 +0300

    assignment2

---
+++ dartassignment2.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = 'Brian Mbithi';
+  String phoneNumber = '+254756911730';
+  String favoriteFood = 'pizza';
+  String email = 'yobra4283@gmail.com';
+  print(
+      'My name is $name. My phone number is $phoneNumber. My favorite food is $favoriteFood. You can reach me at $email.');
+}
