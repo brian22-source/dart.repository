50c9bb1d18153037e52215c6542b523ecb3bba90
19carty <143806608+19carty@users.noreply.github.com>
2023-09-15 16:08:24 +0300

    submission assignment

---
+++ submissionassignment.dart
@@ -0,0 +1,25 @@
+void main() {
+  // int data type example
+  int age = 25;
+  print('Age: $age');
+
+  // double data type
+  double price = 10.99;
+  print('Price: \$${price.toStringAsFixed(2)}');
+
+  // String data type
+  String name = 'Brian Mbithi';
+  print('Name: $name');
+
+  // List data type
+  List<int> numbers = [1, 2, 3, 4, 5];
+  print('Numbers: $numbers');
+
+  // Map data type example
+  Map<String, String> fruits = {
+    'apple': 'red',
+    'banana': 'yellow',
+    'orange': 'orange'
+  };
+  print('Fruits: $fruits');
+}
