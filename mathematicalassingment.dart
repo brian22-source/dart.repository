7b279f40b309952accea24b0fc4076d4118f49eb
19carty <143806608+19carty@users.noreply.github.com>
2023-09-17 13:42:18 +0300

    mathematical functions

---
+++ mathematicalassingment.dart
@@ -0,0 +1,26 @@
+//Perform all mathematical operations using two variables:
+
+void main() {
+  int num1 = 10;
+  int num2 = 5;
+
+//Addition: `int addition = num1 + num2;`
+
+  int addition = num1 + num2;
+
+//Subtraction: `int subtraction = num1 - num2;`
+  int subtraction = num1 - num2;
+
+  //Multiplication: `int multiplication = num1 * num2;`
+  int multiplication = num1 * num2;
+
+//Division: `double division = num1 / num2;
+  double division = num1 / num2;
+  int modulus = num1 % num2;
+
+  print("Addition: $addition");
+  print("Subtraction: $subtraction");
+  print("Multiplication: $multiplication");
+  print("Division: $division");
+  print("Modulus: $modulus");
+}
