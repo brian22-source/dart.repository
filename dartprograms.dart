4d2d7ac933142c8ae5dcacb61291d87315eeb190
19carty <143806608+19carty@users.noreply.github.com>
2023-09-17 13:48:51 +0300

    dart programs

---
+++ dartprograms.dart
@@ -0,0 +1,21 @@
+//Perform any Dart programs using each data type:
+
+void main() {
+  int age = 25;
+  double height = 1.75;
+  String name = "Brian Mbithi";
+  bool isEmployed = true;
+  List<String> hobbies = ["Reading", "Cooking", "Playing Guitar"];
+  Map<String, dynamic> person = {
+    "name": "Jane",
+    "age": 28,
+    "isStudent": false,
+  };
+
+  print("Age: $age");
+  print("Height: $height");
+  print("Name: $name");
+  print("Employment Status: $isEmployed");
+  print("Hobbies: $hobbies");
+  print("Person: $person");
+}
